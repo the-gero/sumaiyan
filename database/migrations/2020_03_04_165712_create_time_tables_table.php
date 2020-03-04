6f016a168a529a87bfb7d9eb1814acd055feda30
the-gero <47275336+the-gero@users.noreply.github.com>
2020-03-04 22:38:34 +0530

    Time tables table

---
+++ database/migrations/2020_03_04_165712_create_time_tables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_tables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('department');
+            $table->string('batch');
+            $table->string('day');
+            $table->string('time');
+            $table->string('subject');
+            $table->string('faculty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_tables');
+    }
+}
